17f75becb30cc6a2a5f4e2d94cf8e5a3b965cfe6
adityapandey03012003@gmail.com <104114546+03Adi@users.noreply.github.com>
2024-02-20 08:20:27 +0530

    #Day19

---
+++ Day19/ABCtriangle.java
@@ -0,0 +1,16 @@
+package Day19;
+import java.util.*;
+public class ABCtriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
