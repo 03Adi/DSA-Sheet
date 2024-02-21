fcdecac4bde8550889f505f5f869dbe240491306
adityapandey03012003@gmail.com <104114546+03Adi@users.noreply.github.com>
2024-02-21 10:11:53 +0530

    #Day21

---
+++ Day21/Binarytriangle.java
@@ -0,0 +1,13 @@
+package Day21;
+import java.util.Scanner;
+public class Binarytriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            
+        }
+    }
+}
