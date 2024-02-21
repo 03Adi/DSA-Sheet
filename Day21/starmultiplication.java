c1feceeed38307b7730241e4ef5c214a53d0c1e2
adityapandey03012003@gmail.com <104114546+03Adi@users.noreply.github.com>
2024-02-21 09:57:16 +0530

    #Day21

---
+++ Day21/starmultiplication.java
@@ -0,0 +1,11 @@
+package Day21;
+import java.util.*;
+public class starmultiplication {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n=sc.nextInt();
+
+        
+    }
+}
