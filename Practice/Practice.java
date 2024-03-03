cf38bde93a9f923322a010ba10051e09f66cd66e
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-03-03 22:39:54 +0530

    #Day32

--- Practice/Practice.java
+++ Practice/Practice.java
@@ -1,30 +1,41 @@
-package Practice;
-import java.util.*;
-public class Practice {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter target element: ");
-        int x=sc.nextInt();
+// package Practice;
+// import java.util.*;
+// public class Practice {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter target element: ");
+//         int x=sc.nextInt();
 
-        System.out.println("Enter array size: ");
-        int n=sc.nextInt();
+//         System.out.println("Enter array size: ");
+//         int n=sc.nextInt();
 
-        int[] arr=new int[n];
-        System.out.println("Enter elements in array: ");
-        for(int i=0;i<arr.length;i++){
-            arr[i]=sc.nextInt();
-        }
+//         int[] arr=new int[n];
+//         System.out.println("Enter elements in array: ");
+//         for(int i=0;i<arr.length;i++){
+//             arr[i]=sc.nextInt();
+//         }
 
-        boolean flag=false;
-        for(int i=0;i<arr.length;i++){
-            if(x==arr[i])
-                flag=true;//means that element founded----
-            break;
-        }
-        if(flag==true)
-            System.out.println("Element found");
+//         boolean flag=false;
+//         for(int i=0;i<arr.length;i++){
+//             if(x==arr[i])
+//                 flag=true;//means that element founded----
+//             break;
+//         }
+//         if(flag==true)
+//             System.out.println("Element found");
         
-        else
-        System.out.println("Element not found");
-    }
-}
+//         else
+//         System.out.println("Element not found");
+//     }
+// }
+
+// package Practice;
+// public class Practice{
+//     public static void main(String[] args) {
+//         int[] arr={1,0,0,1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,1,1,1,};
+
+//         int length=arr.length;
+
+//         System.out.println(length);
+//     }
+// }
