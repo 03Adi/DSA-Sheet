fc97ec9781472881361fe1a575544bcc34641d1f
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-03-04 21:53:26 +0530

    #Day33

--- Day32/method2.java
+++ Day32/method2.java
@@ -1,5 +1,18 @@
 package Day32;
 
 public class method2 {
-    
+    public static void main(String[] args) {
+        int[] arr={1,0,0,1,1,0,1,0,0,0,1};
+        int n=arr.length;
+
+        int noofzeros=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0)
+            noofzeros++;
+        }
+        for(int i=0;i<n;i++){
+            if(i<noofzeros) arr[i]=0;
+            else arr[i]=1;
+        }
+    }
 }
