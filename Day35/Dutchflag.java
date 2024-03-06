bc44fc6920103f12e0cae6df45d62a82d12a096c
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-03-06 23:58:05 +0530

    #Day35

--- Day35/Dutchflag.java
+++ Day35/Dutchflag.java
@@ -6,6 +6,27 @@ package Day35;
 public class Dutchflag {
 
     public static void main(String[] args) {
-        int[] arr={};
+        int[] arr={0,0,0,1,2,1,0,1,0,1,0,2,2,1};
+        
+        int noofzeros=0;
+        int noofones=0;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0) noofzeros++;
+            if(arr[i]==1) noofones++;
+
+        }
+        for(int i=0;i<arr.length;i++){
+            if(i<noofzeros) arr[i]=0;
+            else if(i<noofzeros+noofones) arr[i]=1;
+            else arr[i]=2;
+        }
+        // for(int ele:arr){
+        //     System.out.print(ele+" ");
+        // }
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
