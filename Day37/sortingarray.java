9014157480f4e0128a0b69635f624285bf4a717b
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-03-09 07:56:54 +0530

    #Day38

--- Day37/sortingarray.java
+++ Day37/sortingarray.java
@@ -1,7 +1,18 @@
 package Day37;
 
+import java.util.Arrays;
+
 public class sortingarray {
     public static void main(String[] args) {
-        
+        int[] arr={1,0,0,2,1,0,2,2,0,1,0,0,1};
+        for(int ele:arr){
+            System.out.print(ele+" ");
+        }
+        Arrays.sort(arr);
+        System.out.println();
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
