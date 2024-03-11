bfa128dd3edf74d4f3b9acfec9a95cb7f329c30e
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-03-12 00:01:46 +0530

    #Day40

--- Day39/mergesort.java
+++ Day39/mergesort.java
@@ -4,5 +4,7 @@ public class mergesort {
     public static void main(String[] args) {
         // // In this method basically array sorted in the form of merge sort---- 
         // // Merge Sorted array-------
+
+        
     }
 }
---
+++ Day40/nextgreatestno.java
@@ -0,0 +1,7 @@
+package Day40;
+// // #Day 40 next greatest no----
+public class nextgreatestno {
+    public static void main(String[] args) {
+        
+    }
+}
