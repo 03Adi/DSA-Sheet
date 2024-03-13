1fd903a97599893729f07281b3b05034066da10d
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-03-13 23:30:19 +0530

    Day42

---
+++ Day42/mergearray.java
@@ -0,0 +1,9 @@
+package Day42;
+
+public class mergearray {
+    public static void main(String[] args) {
+        // // baisically using merge sort method to sort the array-------
+
+        
+    }
+}
