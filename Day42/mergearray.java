c59069a5da27953cddad84b362bc87ad3bcc3980
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-03-15 14:37:48 +0530

    #Day44

--- Day42/mergearray.java
+++ Day42/mergearray.java
@@ -6,23 +6,27 @@ public class mergearray {
 
         int[] a={11,23,33,45,65};
         int[] b={26,54,63,62,81,94};
-        int[] c=new int[a.length+b.length];
+        // int m=new int[a.length+b.length];
+        // int[] c=new int[m+n];
+        int m=a.length;
+        int n=b.length;
+        int[] c=new int[m+n];
         int i=0,j=0;k=0;
 
         while(i<a.length && j<b.length){
             if(a[i]<=b[j]){
                 c[k]=a[i];
                 i++;
-                k++;
 
             }
             else{
-                c[k]=b[i];
+                c[k]=b[j];
                 j++;
             }
             k++;
 
         }
+        if
 
 
     }
