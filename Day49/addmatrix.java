bf95249dcd570aa56d04b6c4a9f43006893f86fb
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-03-20 23:43:56 +0530

    #Day49

--- Day49/addmatrix.java
+++ Day49/addmatrix.java
@@ -2,6 +2,26 @@ package Day49;
 
 public class addmatrix {
     public static void main(String[] args) {
+        int[][] a={{1,9,3},{2,3,4},{6,2,1}};
+        int[][] b={{9,7,4},{3,5,6},{4,6,4}};
         
+
+        int m=a.length;
+        int n=b[0].length;
+        int[][] res=new int[m][n];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                res[i][j]=a[i][j]+b[i][j];
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(res[i][j]+" ");
+            }
+            System.out.println();
+        }
+
     }
 }
