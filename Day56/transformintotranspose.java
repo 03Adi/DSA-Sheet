4dd0a1869f3c450fba7d4c5014c1c6600f5f3461
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-03-27 12:45:24 +0530

    #Day56

--- Day56/transformintotranspose.java
+++ Day56/transformintotranspose.java
@@ -18,5 +18,20 @@ public class transformintotranspose {
     public static void main(String[] args) {
         int[][] arr={{1,2,3},{4,5,6},{7,8,9}};
         print(arr);
+
+        System.out.println("After Array tranfrorm into Transpose");
+
+        System.out.println();// basically for next line
+
+        int m=arr.length;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<=i;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+        print(arr);
     }
 }
