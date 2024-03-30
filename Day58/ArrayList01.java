e7c4c819d8a56bb6658b1a43cc04cb90eb2ea83f
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-03-31 03:31:34 +0530

    #Day60

--- Day58/ArrayList01.java
+++ Day58/ArrayList01.java
@@ -3,6 +3,25 @@ import java.util.*;
 public class ArrayList01 {
     public static void main(String[] args) {
         List<Integer> arr=new ArrayList<>();
-        
+        arr.add(10);arr.add(20);arr.add(30);
+
+        List<Integer> arr1=new ArrayList<>();
+        arr1.add(40);arr1.add(50);
+
+        List<Integer> arr2=new ArrayList<>();
+        arr2.add(60);
+
+        List<List<Integer>> store=new ArrayList<>();// basically is used to store the 2 array and multiple aray at a same time
+        store.add(arr);
+        store.add(arr1);
+        store.add(arr2);
+
+        // this loop is print the full length of l name of array
+        for (int i = 0; i < store.size(); i++) {
+            System.out.println(store.get(i));
+        }
+                    // print and get specific no of array element then we use this technique---
+
+        //   System.out.print(store.get(1).get(2));          
     }
 }
