442d4a59d886a604ec96a1c4c16538cc897fc814
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-03-31 23:40:28 +0530

    #Day60

--- Day58/ArrayList01.java
+++ Day58/ArrayList01.java
@@ -17,11 +17,24 @@ public class ArrayList01 {
         store.add(arr2);
 
         // this loop is print the full length of l name of array
-        for (int i = 0; i < store.size(); i++) {
-            System.out.println(store.get(i));
-        }
+        // for (int i = 0; i < store.size(); i++) {
+        //     System.out.println(store.get(i));
+        // }
                     // print and get specific no of array element then we use this technique---
 
-        //   System.out.print(store.get(1).get(2));          
+        //   System.out.print(store.get(1).get(2));     
+        
+        
+
+        // this is my way to print the array in easy and cool form ----
+        
+        // for (int i = 0; i < store.size(); i++) {
+        //     List<Integer> a=store.get(i);
+        //     for (int j = 0; j <a.size(); j++) {
+        //         System.out.print(a.get(j)+" ");
+                
+        //     }
+        //     System.out.println();
+        // }
     }
 }
---
+++ Day60/Pascals.java
@@ -0,0 +1,8 @@
+package Day60;
+
+public class Pascals {
+        public static void main(String[] args) {
+        
+    }
+    
+}
