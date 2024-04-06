b8b35d611e34c1c521469bd9da09c2c57e3e3f9b
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-04-06 13:13:21 +0530

    #Day66

---
+++ Day66/positiveandnegative.java
@@ -0,0 +1,16 @@
+package Day66;
+
+public class positiveandnegative {
+    public static void main(String[] args) {
+        int num=5;
+
+        if(num>0){
+            System.out.println("The num is Positive: "+num);
+        }
+        else if(num<0){
+            System.out.println("The num is Negative: "+num);
+        }
+        else
+        System.out.println("The num is zero: "+num);
+    }
+}
