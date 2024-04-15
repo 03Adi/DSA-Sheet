1a330694771f59d31bbdfbeeee26cb3653aad39d
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-04-15 19:12:45 +0530

    #Day75 String methods--

---
+++ Day75/StringImp.java
@@ -0,0 +1,31 @@
+// package Day75;
+
+// public class StringImp {
+    //// Basically now we are using the concept of lower case and upper case in String ---
+
+// public static void main(String[] args) {
+//     String s="Aditya Parkash Pandey";
+
+//     System.out.println(s.toLowerCase());
+// }
+
+
+//// Basically to make Uppercase---
+
+// public static void main(String[] args) {
+//     String s="Aditya Prakash Pandey";
+//     System.out.println(s.toUpperCase());
+// }
+
+
+    //// Basically its a concat method in String----
+    // public static void main(String arg[]){
+    //     String a="Aditya";
+
+    //     //now we are using the concat method to add something after the String a----
+
+    //     String A=a.concat(" Prakash");
+    //     System.out.println(A);
+    // }
+// }
+
