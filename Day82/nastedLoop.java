d8fd67dad9990328eaa9080ec1d4fc9d0b96b51f
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-04-22 09:48:04 +0530

    Day82

---
+++ Day82/nastedLoop.java
@@ -0,0 +1,18 @@
+package Day82;
+import java.util.*;
+
+public class nastedLoop {
+    public static void main(String arg[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no: ");
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+
+        for (int i = 0; i <n; i++) {
+           for (int j = 0; j < m; j++) {
+            System.out.println("Hellow Aditya");
+           }
+            
+        }
+    }
+}
