c5152aa2e8f5c865008cdcd14d7b958920ecb9de
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-04-25 14:03:48 +0530

    #Day85

---
+++ Day85/givenrangesum.java
@@ -0,0 +1,13 @@
+package Day85;
+
+public class givenrangesum {
+    public static void main(String arg[]){
+        int a1=3;
+        int a2=9;
+        int sum=0;
+
+        for(int i=a1;i<=a2;i++)
+        sum+=i;
+        System.out.println("The sum is given range is: "+sum);
+    }
+}
