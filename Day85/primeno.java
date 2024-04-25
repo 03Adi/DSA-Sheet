448a5006b171d5e2a2c81390619d49c5382d44b2
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-04-25 13:36:18 +0530

    #Day85

--- Day85/primeno.java
+++ Day85/primeno.java
@@ -1,7 +1,26 @@
 package Day85;
+import java.util.*;
 
 public class primeno {
-    public static void main(String[] args) {
+
+    static boolean isprime(int num){
+        if(num<=1)
+            return false;
+
+            for(int i=2;i*i<=num;i++){
+                return false;
+            }
+            return true;
         
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter No: ");
+        int num=sc.nextInt();
+
+        if(isprime(num))
+        System.out.println(num+" is prime no");
+        else
+        System.out.println(num+" is not prime no");
     }
 }
