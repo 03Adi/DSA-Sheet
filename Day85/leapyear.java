32bf488645741a52d8f59fa6d744f2cec885075c
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-04-25 14:07:59 +0530

    #Day85

---
+++ Day85/leapyear.java
@@ -0,0 +1,12 @@
+package Day85;
+
+public class leapyear {
+    public static void main(String[] args) {
+        int year=2024;
+        if((year%400==0)||(year%100!=0 && year%4==0)){
+            System.out.println("Given year is leap year: "+year);
+        }
+        else
+        System.out.println("Given year is not leap year: "+year);
+    }
+}
