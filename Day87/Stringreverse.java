eec465e7c06d39436ab1c9d6c3744465b0f0579b
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-04-27 16:36:37 +0530

    #Day87

---
+++ Day87/Stringreverse.java
@@ -0,0 +1,25 @@
+package Day87;
+
+public class Stringreverse {
+    public static void main(String[] args) {
+        
+        // // using built in function----
+        // StringBuilder s=new StringBuilder("Aditya");
+        // System.out.println(s);
+
+        // // now reverse---
+        // s.reverse();
+        // System.out.println(s);
+
+        // // without using built in function----
+        // StringBuilder s=new StringBuilder("Aditya");
+        // System.out.println(s);
+
+        // String res="";
+        // for(int i=s.length()-1;i>=0;i--){
+        //     res+=s.charAt(i);
+        // }
+        // System.out.println(res);
+
+    }
+}
